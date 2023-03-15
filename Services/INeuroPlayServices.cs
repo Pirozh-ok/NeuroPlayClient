178519f3783aaac65ad957a383370f58d7da0f1d
Ivan Vorotnikov <studio154@bk.ru>
2023-03-16 00:09:03 +0700

    Start implement methods for work with NeuroPlay

---
+++ Services/INeuroPlayServices.cs
@@ -0,0 +1,9 @@
+﻿namespace NeuroPlayClient.Services {
+    internal interface INeuroPlayServices {
+        bool IsConnected();
+        int GetCountDevices();
+        void StartRecord();
+        void StopRecord();
+        void AddMarker();
+    }
+}
---
+++ Services/NeuroPlayServices.cs
@@ -0,0 +1,30 @@
+﻿using NeuroPlayClient.Resources;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NeuroPlayClient.Services {
+    public class NeuroPlayServices : INeuroPlayServices {
+
+        public void AddMarker() {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<int> GetCountDevices() {
+            using (var client = new HttpClient()) {
+                var result = await client.GetAsync(Urls.GetCountDevices);
+            };
+        }
+
+        public bool IsConnected() {
+            throw new System.NotImplementedException();
+        }
+
+        public void StartRecord() {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopRecord() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
